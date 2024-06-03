fba423e9996f7a88fcbdae343af3d4a458716576
jieun <jieun_0330@naver.com>
2024-06-03 21:10:30 +0900

    init: initial commit

---
+++ TCA_Practice/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TCA_Practice
+//
+//  Created by 박지은 on 6/3/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TCA_Practice/TCA_PracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  TCA_PracticeApp.swift
+//  TCA_Practice
+//
+//  Created by 박지은 on 6/3/24.
+//
+
+import SwiftUI
+
+@main
+struct TCA_PracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
