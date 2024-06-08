852fbf740ee83dd85121f17705980d48e701c1fc
jieun <jieun_0330@naver.com>
2024-06-08 11:50:01 +0900

    chore: TCA 라이브러리 추가

---
+++ TCA_Practice/CounterFeature.swift
@@ -0,0 +1,18 @@
+//
+//  CounterFeature.swift
+//  TCA_Practice
+//
+//  Created by 박지은 on 6/8/24.
+//
+
+import SwiftUI
+
+struct CounterFeature: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CounterFeature()
+}
